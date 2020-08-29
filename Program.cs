e7405084208c646cca0b4834f1e78ca263b9c589
AlecTech <eremeevoleg@gmail.com>
2020-08-29 12:53:30 -0600

    Initial commit relocating to personal github account

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;//dictionary obj
+using System.Linq; //Dictionary.Key method
+using System.Text; //has String builder obj
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Title: C# Letter Remover Practice
+            //Purpose: Allows user to enter string and then lets to remove any character 
+            //Author: Oleg Eremeev
+            //Date: 2020 Aug 12
+
+            //string runApp = true;
+
+                //while (runApp = Console.ReadKey())
+                //{            
+            string result = "";
+            Console.WriteLine("Please enter any string");         
+            string source = Console.ReadLine();
+
+            Console.WriteLine("Enter what you need to delete");
+            string remove = Console.ReadLine();
+
+
+
+
+              /*
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] != userChar[0])
+                {
+                    output += userInput[i];
+                }
+            }
+            */
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == remove[0])
+                {
+                    source = source.Remove(i, 1);
+                    i--;
+                }
+            }
+            result = source;
+
+
+            Console.WriteLine($"Your modified string is on the next line:\n{result}");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Trello Link
+https://trello.com/b/LHJOnmUf/c-letter-remover-practice
